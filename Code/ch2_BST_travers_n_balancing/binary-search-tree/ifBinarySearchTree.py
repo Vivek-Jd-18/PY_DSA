7ecd95a11244c14ab408c3207f96fc6bb35faba5
Vivek-Jd-18 <vivekdhage.vhits@gmail.com>
2024-03-28 11:26:19 +0530

    learning: identify if a tree is a binary tree

--- Code/ch2_BST_travers_n_balancing/binary-search-tree/ifBinarySearchTree.py
+++ Code/ch2_BST_travers_n_balancing/binary-search-tree/ifBinarySearchTree.py
@@ -1,9 +1,35 @@
+import sys
 class Tree:
     def __init__(self,key) -> None:
         self.key = key
         self.right = None
         self.left = None
 
-tree_data = ((1,2,3),4,(5,6,7))
+# tree_data = ((1,2,3),4,(5,6,7))
+# tree_data = ((1,3,2),4,(5,6,7))
+# tree_data = (4,7,9)
+tree_data = (None, 1, 6)
 
-def if_tree_isbst(tree:Tree):
+def parse_tuple(nodes:tuple)-> Tree:
+    if isinstance(nodes, tuple) and len(nodes) == 3:
+        node:Tree = Tree(nodes[1])
+        node.left = parse_tuple(nodes[0])
+        node.right = parse_tuple(nodes[2])
+    elif nodes is None:
+        node:Tree = None
+    else:
+        node:Tree = Tree(nodes)
+    return node
+
+tree0 = parse_tuple(tree_data)
+
+# check if tree is bst or not using general approach
+def is_bst(node:Tree, min_val:int, max_val:int) -> bool:
+    print((node.key if node is not None else None), min_val, max_val)
+    if node == None:
+        return True
+    if node.key >= max_val or node.key <=min_val:
+        return False
+    return (is_bst(node.right, node.key, max_val) and is_bst(node.left, min_val, node.key))
+
+print(is_bst(tree0, -2**31-1, 2**31))
