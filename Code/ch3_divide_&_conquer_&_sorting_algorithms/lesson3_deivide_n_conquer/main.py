d74c46230222d3f6adf169aa61028429278f95be
Vivek-Jd-18 <vivekdhage.vhits@gmail.com>
2024-04-23 19:15:19 +0530

    ..............

---
+++ Code/ch3_divide_&_conquer_&_sorting_algorithms/lesson3_deivide_n_conquer/main.py
@@ -0,0 +1,7 @@
+# Divide and Conquer
+# to perform sorting we will use divide and conquer technique, which has general steps:
+
+# 1. Divide inputs into 2 roughly equal parts
+# 2. Recursevily solve each part
+# 3. Combine results of both to solve the main problem
+# 4. include terminating conditions for small inputs
