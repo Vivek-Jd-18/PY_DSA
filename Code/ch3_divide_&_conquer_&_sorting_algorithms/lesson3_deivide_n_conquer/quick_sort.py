4d3f06096dad1c4964de138ebeb984b0a6131785
Vivek-Jd-18 <vivekdhage.vhits@gmail.com>
2024-04-30 19:15:04 +0530

    learning: quick sort

--- Code/ch3_divide_&_conquer_&_sorting_algorithms/lesson3_deivide_n_conquer/quick_sort.py
+++ Code/ch3_divide_&_conquer_&_sorting_algorithms/lesson3_deivide_n_conquer/quick_sort.py
@@ -1,9 +1,17 @@
 # Quick-Sort (Divide n Conquer)
 
-def quick_sort(list, pivot)->list[int]:
-    pass
+# def quick_sort(list, pivot)->list[int]:
+#     pass
 
+def quicksort(arr):  
+    if len(arr) < 2:  
+        return arr  
+    pivot = arr[0]  
+    left = [x for x in arr[1:] if x < pivot]  
+    right = [x for x in arr[1:] if x >= pivot]  
+    return quicksort(left) + [pivot] + quicksort(right)  
 
+print(quicksort([3, 5, 7, 2, 4, 6, 8]),"res")
 
 #  3, 5, 7, 2, 4, 6, '8'
 #  ['3', 5, 7, 2, 4, 6, 8] []    
